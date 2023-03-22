6dc5825ed6309182fc04b9d4007586abe02b60f0
SysAdminOkuns <sysad.okundaye@gmail.com>
2023-03-22 13:03:32 -0400

    Added a comment to the main method while testing git

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Added a comment to the main method
 
 //        Check typecasting examples
 //        RandomExamples.typeCastingExample();
